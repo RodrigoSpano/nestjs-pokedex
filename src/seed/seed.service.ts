ad7b439d6cae346d1eb9edd0ea57506630839ec4
RodrigoSpano <rorrospano17@gmail.com>
2023-11-10 17:20:01 -0300

    seed configured

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { IPokemonResponse } from './interfaces/pokemon-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<IPokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=120',
     );
-    data.results.forEach(({ name, url }) => {
+
+    const pokemonsToInsert: { name: string; no: number }[] = [];
+    data.results.forEach(async ({ name, url }) => {
       const segment = url.split('/');
       const no = +segment[segment.length - 2];
-      console.log(name, no);
+      pokemonsToInsert.push({ name, no });
     });
-    // return data.results;
+    await this.pokemonModel.insertMany(pokemonsToInsert);
+    return 'seed executed';
   }
 }
